01e0fd6972e0f596ee606076e33dd64bb299b50c
1adored1 <perekrutq@gmail.con>
2024-09-03 20:40:32 +0500

    add random falling balls

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ mp_draw = mp.solutions.drawing_utils
 
 cap = cv2.VideoCapture(0)
 
+balls = []
+ball_radius = 30
+
+def create_ball():
+    x = random.randint(ball_radius, 620)
+    y = 0
+    return [x, y]
+
 while cap.isOpened():
     success, frame = cap.read()
     if not success:
@@ -19,9 +27,26 @@ while cap.isOpened():
 
     frame_height, frame_width, _ = frame.shape
 
+    hand_pos = []
     if landmarks.multi_hand_landmarks:
         for hand_landmarks in landmarks.multi_hand_landmarks:
+            for id, lm in enumerate(hand_landmarks.landmark):
+                x, y = int(lm.x * frame_width), int(lm.y * frame_height)
+                hand_pos.append((x, y))
             mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+    
+    if random.random() < 0.05:
+        balls.append(create_ball())
+
+    remaining_balls = []
+    for i in range(len(balls)):
+        balls[i][1] += 5
+        cv2.circle(frame, (balls[i][0], balls[i][1]), ball_radius, (0, 255, 0), -1)
+
+        if balls[i][1] - ball_radius < frame_height:
+            remaining_balls.append(balls[i])
+        
+    balls = remaining_balls
 
     cv2.imshow('game', frame)
     
